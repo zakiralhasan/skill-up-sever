9c73f147c65876f5bd859f9475f2ab86d9827d3a
zakiralhasan <zakiralhasan.bd@gmail.com>
2022-10-27 10:34:42 +0600

    course data list added

--- index.js
+++ index.js
@@ -6,13 +6,26 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const courseTitle = require("./data/courseTitle.json");
+const courseDetails = require("./data/courseData.json");
 
 app.get("/", (req, res) => {
   res.send("API server is running");
 });
 
+//course title list send through API
 app.get("/course", (req, res) => {
   res.send(courseTitle);
+});
+
+//course data send through API
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedData = courseDetails.find(
+    (course) => course.category_id === id
+  );
+  res.send(selectedData);
+  console.log(id);
+  console.log(selectedData);
 });
 
 app.listen(port, () => {
